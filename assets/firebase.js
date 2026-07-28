7461f538cd5aed5d1fb0c1084531acd2d53abc51
hzin03071-sudo <hzin03071@gmail.com>
2026-07-28 15:34:14 +0900

    Update firebase.js
    웹 푸시 추가

--- assets/firebase.js
+++ assets/firebase.js
@@ -1,8 +1,21 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.10.0/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/11.10.0/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/11.10.0/firebase-firestore.js";
+import {
+  getMessaging,
+  isSupported
+} from "https://www.gstatic.com/firebasejs/11.10.0/firebase-messaging.js";
+
 import { firebaseConfig } from "./firebase-config.js";
 
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
+
+/*
+  일부 브라우저에서는 웹 푸시를 지원하지 않을 수 있으므로
+  지원 여부를 먼저 확인한 후 Messaging을 연결합니다.
+*/
+export const messaging = await isSupported()
+  ? getMessaging(app)
+  : null;
